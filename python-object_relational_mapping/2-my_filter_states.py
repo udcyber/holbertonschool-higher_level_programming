18c362fa99c3e93210d07f42505cc1263e161145
udcyber <udewaleyne.pro@gmail.com>
2026-06-12 14:44:20 +0200

    take in an argument and display all values in the states table

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" take in an argument and displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in cursor.fetchall()]
