d85331f273d9c77e1a4e333a0c1a24a4de6a9a52
udcyber <udewaleyne.pro@gmail.com>
2026-06-12 14:30:08 +0200

    list all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     curs = db.cursor()
-    rows_number = curs.execute("SELECT * FROM states ORDER BY states.id")
-    for i in range(rows_number):
-        print(curs.fetchone())
+    curs.execute("SELECT * FROM `states` ORDER BY `ìd`")
+    [print(state) for state in curs.fetchall() if state[1][0] == "N"] 
     db.close()
