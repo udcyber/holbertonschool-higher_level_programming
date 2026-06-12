1a1af6fe079df850c3664816b14abe485ae90c29
udcyber <udewaleyne.pro@gmail.com>
2026-06-12 15:30:05 +0200

    take in arguments and display all values in state table

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
-"""script should take 4 arguments: mysql username, mysql password, database name and state name searched"""
-"""should connect to a MySQL server running on localhost at port 3306"""
-"""Results must be sorted in ascending order by states.id"""
+"""a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument
+script should take 4 arguments: mysql username, mysql password, database name and state name searched
+should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id"""
 
 import sys
 import MySQLdb
