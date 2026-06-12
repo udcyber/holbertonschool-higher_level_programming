051db116af1953bb75d84a8c62415853c12224d2
udcyber <udewaleyne.pro@gmail.com>
2026-06-12 15:35:08 +0200

    create class state

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Create the State class"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """class State is created"""
+    __tablename__ = 'states'
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
