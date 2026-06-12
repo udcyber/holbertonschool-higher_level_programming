8e7172c758ebad3124e3f6ccdc6fd40c69e12b01
udcyber <udewaleyne.pro@gmail.com>
2026-06-12 14:34:39 +0200

    list all states with a name starting with N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+lists all states with a name starting with N from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    curs = db.cursor()
+    curs.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in curs.fetchall() if state[1][0] == "N"]
