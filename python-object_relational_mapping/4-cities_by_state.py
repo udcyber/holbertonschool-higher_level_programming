1823712761d40a1b6ba0698879a81a508decf87e
udcyber <udewaleyne.pro@gmail.com>
2026-06-16 11:04:14 +0200

    changed single quote to triple quote (more readable), and fetchone() to fetchall()

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 """
-lists all cities from the database hbtn_0e_4_usa
+List all cities from the database hbtn_0e_4_usa.
 """
 
+import sys
 import MySQLdb
-from sys import argv
 
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    sql = "SELECT cities.id, cities.name, states.name FROM cities\
-    JOIN states ON cities.state_id = states.id ORDER BY cities.id"
-    num_rows = cursor.execute(sql)
-    for i in range(num_rows):
-        print(cursor.fetchone())
+    sql = """SELECT cities.id, cities.name, states.name
+             FROM cities
+             JOIN states ON cities.state_id = states.id
+             ORDER BY cities.id"""
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
     cursor.close()
     db.close()
