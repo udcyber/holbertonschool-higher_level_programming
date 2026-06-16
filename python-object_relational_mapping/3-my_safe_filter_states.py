03f49a5146df1f63d4955493a577ac20e3f608ca
udcyber <udewaleyne.pro@gmail.com>
2026-06-16 11:00:31 +0200

    added commands, changed structure with % and tuple (more secure)

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument
-script should take 4 arguments: mysql username, mysql password, database name and state name searched
-should connect to a MySQL server running on localhost at port 3306
-Results must be sorted in ascending order by states.id"""
+"""
+A script, safe from MySQL injections, that takes in arguments and displays
+all values in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states`")
-    [print(state) for state in cursor.fetchall() if state[1] == sys.argv[4]]
+# %s tells the library to treat the input as data, not code
+# the comma after sys.argv[4] turns the value into a tuple
+    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4],))
+    for state in cursor.fetchall():
+        print(state)
+    db.close()
