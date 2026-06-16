82802c65ffc2aa8362471c294f91ae88962da50f
udcyber <udewaleyne.pro@gmail.com>
2026-06-16 10:08:44 +0200

    small change - 1 more line

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,6 +7,7 @@ where name matches the argument.
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
