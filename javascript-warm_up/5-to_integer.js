9b270fa3d6a414b07ac93c0b388e2907b9fd1a40
udcyber <udewaleyne.pro@gmail.com>
2026-06-30 20:20:45 +0200

    write number if can be converted to an integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,12 +3,11 @@
 // Write "My number: (number)"" if the 1st argument can be converted to an integer.
 // Else, write "Not a number".
 
-// Math.floor to change the number into an integer (fi 89.89 becomes 89)
+// Math.floor to change the number into an integer (fi 89.89 becomes 89).
 const firstArg = Math.floor(Number(process.argv[2]));
 
 if (Number.isInteger(firstArg) === true) {
   console.log(`My number: ${firstArg}`);
-}
-else if (Number.isInteger(firstArg) === false) {
+} else {
   console.log('Not a number');
 }
