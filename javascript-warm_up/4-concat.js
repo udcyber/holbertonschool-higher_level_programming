415ce44a9411923711a44b0d0ac5816f7f67c4d1
udcyber <udewaleyne.pro@gmail.com>
2026-06-30 18:50:53 +0200

    print 2 arguments

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,4 +2,4 @@
 
 // Print two arguments passed, in the following format: "is".
 
-console.log(process.argv[2] + " is " + process.argv[3]);
+console.log(process.argv[2]+ " " + "is" + " " + process.argv[3]);
