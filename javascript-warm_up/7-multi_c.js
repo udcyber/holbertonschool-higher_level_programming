655f3950d6f4861041a94111bf2a0464ac552e98
udcyber <udewaleyne.pro@gmail.com>
2026-06-30 21:02:40 +0200

    declare let inside the function instead

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,14 +2,12 @@
 
 // Print x times "C is fun".
 
-let i;
 const x = Math.floor(Number(process.argv[2]));
 
 if (Number.isInteger(x) === false) {
   console.log('Missing number of occurences');
-}
-else {
-  for (i = 0; i < x; i++) {
+} else {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
 }
