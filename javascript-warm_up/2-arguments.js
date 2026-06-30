3b798a7941debec755efc08fb6db7a934f6a9200
udcyber <udewaleyne.pro@gmail.com>
2026-06-30 18:13:07 +0200

    change syntax, more readable

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,4 +4,10 @@
 
 const argNumber = process.argv.length;
 
-if (argNumber === 2) { console.log('No argument'); } else if (argNumber === 3) { console.log('Argument found'); } else if (argNumber > 3) { console.log('Arguments found'); }
+if (argNumber === 2) {
+  console.log('No argument');
+} else if (argNumber === 3) {
+  console.log('Argument found');
+} else if (argNumber > 3) {
+  console.log('Arguments found');
+}
