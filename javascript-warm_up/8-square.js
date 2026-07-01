11d0ff6f37d0fa6d9441a53c4aa6b38846b09179
udcyber <udewaleyne.pro@gmail.com>
2026-07-01 04:04:39 +0200

    print a square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// Print a square.
+
+const squareSize = Math.floor(Number(process.argv[2]));
+
+if (isNaN(squareSize)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < squareSize; i++) {
+    let row = '';
+    for (let j = 0; j < squareSize; j++) {
+      row += 'X'; 
+    }
+    console.log((row));
+  }
+}
