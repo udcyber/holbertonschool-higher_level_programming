b8d6b69699df5bdb23a5bcf1c774a06a6e691b4e
udcyber <udewaleyne.pro@gmail.com>
2026-07-01 19:54:23 +0200

    update text color

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,4 +1,4 @@
 // Update the text color of the header to red.
 
-const headerColor = document.querySelector('header');
-headerColor.style.color = '#FF0000';
+const myHeader = document.querySelector('header');
+myHeader.style.color = '#FF0000';
