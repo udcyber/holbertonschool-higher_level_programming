e0af6a8d04804cf3f211d78b837f091ce43f17cd
udcyber <udewaleyne.pro@gmail.com>
2026-07-01 05:05:30 +0200

    print a factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+// Print a factorial.
+
+const arg1 = (Number(process.argv[2]));
+
+function printFactorial (arg1) {
+  // Check for 0!
+  // Check if arg1 is not a number.
+  if ((isNaN(arg1)) || (arg1 === 0)) {
+    return 1;
+  }
+  // Recursion: the function calls itself until it reaches 0.
+  else {
+    return arg1 * (printFactorial (arg1 - 1));
+  }
+}
+
+console.log(printFactorial(arg1));
