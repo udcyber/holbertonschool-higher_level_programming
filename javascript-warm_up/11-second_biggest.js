361f183cb9bfb8ca8ce7703096667c7cca712c01
udcyber <udewaleyne.pro@gmail.com>
2026-07-01 13:07:05 +0200

    search 2nd biggest integer in list of arguments

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// Search the second biggest integer in the list of arguments.
+// Assume all arguments can be converted to integer.
+
+// Check if the list is empty or only has one argument.
+if (process.argv.length === 0 || process.argv.length === 1) {
+  console.log(0);
+} else {
+  const argList = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(argList[argList.length - 2]);
+}
